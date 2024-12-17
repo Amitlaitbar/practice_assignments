f85d25bee20ea9210c56d330a5c125f5023cbb98
Amitlaitbar <amitlaitbar@gmail.com>
2024-12-17 18:40:31 +0530

    Added division

--- calculator.js
+++ calculator.js
@@ -1,11 +1,15 @@
 const add = function (value1, value2) {
   return value1 + value2;
-}
+};
 
 const sub = function (value1, value2) {
   return value1 - value2;
-}
+};
 
 const multiplication = function (value1, value2) {
   return value1 * value2;
-}
+};
+
+const divison = function (value1, value2) {
+  return value1 / value2;
+};
